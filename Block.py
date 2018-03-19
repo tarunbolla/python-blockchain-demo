e920fd56f35d276dc0ecbad40d5abce4d6ccee5b
Tarun Bolla <tarunkumar.bolla@gmail.com>
2018-03-18 22:39:27 -0500

    Initial commit

---
+++ Block.py
@@ -0,0 +1,61 @@
+import hashlib as hasher
+import datetime as date
+import re
+
+class Block:
+    dif = re.compile(r"^0000000")
+    def __init__(self, index, timestamp, data, previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.hash_block()
+
+    def hash_block(self):
+        nonce = 1
+        while(True):
+            nonce += 1
+            sha = hasher.sha256()
+            sha.update((str(self.index) +
+                    str(self.timestamp) +
+                    str(self.data) +
+                    str(self.previous_hash) +
+                    str(nonce)).encode())
+            digest = sha.hexdigest()
+            #print(str(nonce) + " -- " + digest)
+            if(self.dif.match(digest)):
+                return digest
+
+starttime = date.datetime.now()
+
+def create_genesis_block():
+    # Manually construct a block with
+    # index zero and arbitrary previous hash
+    return Block(0, date.datetime.now(), "Genesis Block", "0")
+
+def next_block(last_block):
+    this_index = last_block.index + 1
+    this_timestamp = date.datetime.now()
+    this_data = "Hey! I'm block " + str(this_index)
+    this_hash = last_block.hash
+    return Block(this_index, this_timestamp, this_data, this_hash)
+
+# Create the blockchain and add the genesis block
+blockchain = [create_genesis_block()]
+previous_block = blockchain[0]
+
+# How many blocks should we add to the chain
+# after the genesis block
+num_of_blocks_to_add = 1
+
+# Add blocks to the chain
+for i in range(0, num_of_blocks_to_add):
+    block_to_add = next_block(previous_block)
+    blockchain.append(block_to_add)
+    previous_block = block_to_add
+    print("Block #{} has been added to the blockchain!".format(block_to_add.index))
+    print("Hash: {}\n".format(block_to_add.hash))
+
+endtime = date.datetime.now()
+
+print("Program execution time: " + str(endtime - starttime) + " sec");
